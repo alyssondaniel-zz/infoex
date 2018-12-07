62e636a69e645206a89f7cbe4b247d7d2115fcd5
Alysson Daniel Nascimento <alyssondaniel@gmail.com>
2018-12-07 16:14:47 -0300

    Added pudint and rolify

--- Gemfile
+++ Gemfile
@@ -63,3 +63,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'bootstrap', '~> 4.0.0'
 gem 'jquery-rails'
 gem 'devise'
+gem "pundit"
+gem "rolify"
---
+++ app/policies/application_policy.rb
@@ -0,0 +1,49 @@
+class ApplicationPolicy
+  attr_reader :user, :record
+
+  def initialize(user, record)
+    @user = user
+    @record = record
+  end
+
+  def index?
+    @user.has_role? :admin
+  end
+
+  def show?
+    @user.has_role? :admin
+  end
+
+  def create?
+    @user.has_role? :admin
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    @user.has_role? :admin
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    @user.has_role? :admin
+  end
+
+  class Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      scope.all
+    end
+  end
+end
